090be342aa804c2e5067a0722e35a7cc7658bf0b
Gallie83 <keving2913@hotmail.com>
2023-11-13 10:56:05 +0100

    Moved App styling to App.css in styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,27 @@ import ContentCard from './components/ContentCard';
 import Genres from './components/Genres';
 import Movies from './components/Movies';
 import Container from 'react-bootstrap/Container';
+import './styles/App.css'
 
 
-// const Container = styled.div`
+// const ContentContainer = styled.div`
 // display:flex;
-// flex-direction:column;
+// flex-direction:row;
+// flex-wrap: wrap;
+// padding: 2rem;
+// justify-content: space-evenly;
 // `;
-
-const Header = styled.div`
-display:flex;
-flex-direction:row;
-background-color: red;
-color: black;
-padding: 1rem;
-font-size: 3rem;
-font-weight: bold;
-box-shadow: 0 3px 6px 0 #555;
-`;
-
-const ContentContainer = styled.div`
-display:flex;
-flex-direction:row;
-flex-wrap: wrap;
-padding: 2rem;
-justify-content: space-evenly;
-`;
 
 
 function App() {
 
   return <>
-    <nav>Movies</nav>
-    <Header>
+    <nav className='nav'>
       Movie Showdown
-    </Header>
-    <Container fluid>
+    </nav>
 
-      <Movies></Movies>
-    </Container>
+    <Movies></Movies>
+    {/* </Container> */}
     {/* <Genres></Genres> */}
     {/* <ContentContainer> */}
     {/* <ContentCard /> */}
