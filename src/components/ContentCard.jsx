9324ad229e8c820c31c09194b4de9dfc905a795d
Gallie83 <keving2913@hotmail.com>
2023-12-16 01:07:41 +0100

    Can't compare user movie choices, if different movies are picked it stores them in seperate arrays

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -1,13 +1,49 @@
-import { styled } from "styled-components";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { img500x500 } from "../utils/imageLink";
 import '../styles/ContentCard.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 
-function ContentCard({ movie }) {
-    return <div className="content-card" >
+const ContentCard = ({ movie, showdownResult, setShowdownResult }) => {
+    // Stores users movie choice
+    const [userChoices, setUserChoices] = useState([]);
+
+
+    // useEffect(() => {
+    //     if (userChoices.length > 1) {
+    //         console.log(userChoices)
+    //         const selectedObject = userChoices[userChoices.length - 1];
+    //         const previousObject = userChoices[userChoices.length - 2];
+    //         if (selectedObject === previousObject) {
+    //             console.log('W');
+    //             console.log(userChoices);
+
+    //             setShowdownResult('W')
+    //         } else {
+    //             console.log('L');
+    //             setShowdownResult('L')
+    //             console.log(userChoices);
+    //         }
+    //     }
+    //     // Update the previous state reference
+    //     prevUserChoicesRef.current = userChoices;
+    // }, [userChoices]);
+
+    const handleObjectSelect = (selectedObject) => {
+        if (userChoices.length > 0 && selectedObject !== userChoices[0]) {
+            console.log('L');
+            setShowdownResult('L')
+            console.log(userChoices);
+        } else if (userChoices.length > 0 && selectedObject === userChoices[0]) {
+            // Checks if movie choices were a match
+            setUserChoices((prevChoices) => [...prevChoices, selectedObject]);
+            console.log('W')
+            console.log(userChoices)
+        }
+    };
+
+    return <div className="content-card" onClick={() => handleObjectSelect(movie?.title)}>
         <img className="movie-poster img-fluid" src={`${img500x500}/${movie?.poster_path}`} />
         <div className="movie-title">{movie?.title}</div>
         <div className="movie-info">
@@ -15,6 +51,7 @@ function ContentCard({ movie }) {
             <span><FontAwesomeIcon icon={faStar} />{movie?.vote_average}</span>
         </div>
     </div>;
+
 }
 
 export default ContentCard;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -11,40 +11,8 @@ const Movies = () => {
     const [selectedGenres, setSelectedGenres] = useState([]);
     const [selectedMovies, setSelectedMovies] = useState([]);
     const [randomNumber, setRandomNumber] = useState([]);
+    const [showdownResult, setShowdownResult] = useState([]);
 
-    // Genres user chooses to filter by
-    // const genreIds = genreID(selectedGenres);
-
-    // FETCH MOVIES
-    // const fetchMovies = async () => {
-    //     try {
-    //         const data = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_REACT_APP_MOVIE_KEY}&language=en-US&with_genres=${genreIds}`);
-    //         setMovies(data?.data.results);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     fetchMovies();
-    // }, [selectedGenres]);
-
-
-    // Trying to return 5 random movies to display but array returns as empty
-    // const handleRandomMovie = () => {
-    //     const random = [];
-    //     for (let i = 0; i < 5; i++) {
-    //         random.push(movies.splice(Math.floor(Math.random() * movies.length), 1))
-    //         console.log(random);
-    //     }
-    //     setRandomMovies(random);
-    // }
-
-    // useEffect(() => {
-    //     handleRandomMovie();
-    // }, []);
-
-    // console.log(movies.length);
     return (
         <div className='d-flex m-2'>
             <Genres
@@ -64,7 +32,11 @@ const Movies = () => {
                 {selectedMovies.map(selectedMovie => (
 
                     <Container className='content-container m-1'>
-                        <ContentCard movie={selectedMovie} key={selectedMovie?.id} />
+                        <ContentCard
+                            movie={selectedMovie}
+                            showdownResult={showdownResult}
+                            setShowdownResult={setShowdownResult}
+                            key={selectedMovie?.id} />
                     </Container>
 
                 ))}
